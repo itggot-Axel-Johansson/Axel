1c2ebe78d7aeed42c026924ffee60c805e734de9
itggot-Axel-Johansson <axel.johansson@itggot.se>
2017-04-17 21:18:36 +0200

    das

--- axel/site/js/js.js
+++ axel/site/js/js.js
@@ -7,12 +7,43 @@ function boots() {
         element.classList.toggle('innan');
     });
 }
-function toggleresultat() {
+function allsvenskan() {
     var elements = document.querySelectorAll('.efter2')
     elements.forEach(function (element) {
         element.classList.toggle('innan2');
     });
 }
+function superettan() {
+    var elements = document.querySelectorAll('.efter3')
+    elements.forEach(function (element) {
+        element.classList.toggle('innan3');
+    });
+}
+function bundesliga() {
+    var elements = document.querySelectorAll('.efter4')
+    elements.forEach(function (element) {
+        element.classList.toggle('innan4');
+    });
+}
+function bundesliga2() {
+    var elements = document.querySelectorAll('.efter5')
+    elements.forEach(function (element) {
+        element.classList.toggle('innan5');
+    });
+}
+function seriea() {
+    var elements = document.querySelectorAll('.efter6')
+    elements.forEach(function (element) {
+        element.classList.toggle('innan6');
+    });
+}
+function serieb() {
+    var elements = document.querySelectorAll('.efter7')
+    elements.forEach(function (element) {
+        element.classList.toggle('innan7');
+    });
+}
+
 function tabort() {
     var elements = document.querySelectorAll('.efter')
     elements.forEach(function (element) {
